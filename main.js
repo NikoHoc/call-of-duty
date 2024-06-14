17fbe63253f178bd46fba2670da92d935f0172ab
xdKeto <128332966+xdKeto@users.noreply.github.com>
2024-06-14 19:12:14 +0700

    added cameras and TODO

--- main.js
+++ main.js
@@ -1,20 +1,50 @@
 import * as THREE from "three";
-import { createMap } from './map.js';
-import { FreeCam } from './freeCam.js';
+import { createMap } from "./map.js";
+import { FreeCam } from "./freeCam.js";
+import { SceneCam1 } from "./sceneCam1.js";
+import { SceneCam2 } from "./sceneCam2.js";
+import { SceneCam3 } from "./sceneCam3.js";
+
+/*
+Camera: 1 = FreeCam (WASD, QE), 2 = sceneCam1, 3 = sceneCam2, 4 = sceneCam3  
+
+========= TODO =========
+NOTE: juga perhatiin rubrik https://docs.google.com/spreadsheets/d/1JRIOTHHMH6L2lxS_MeSreq2OwxJVVn6J3AD9jrN0tYI/edit?usp=sharing
+ - ENVIROMENT:
+  -> tambah asset di map supaya kyk map asli (mobil tengah, bushes, etc)
+
+ - LIGHTING:
+  -> street light
+  -> sun/moon
+  -> shadows
+
+ - CHARACTER:
+  -> masukin char
+  -> script
+  -> senjata
+
+- CAMERA
+  -> camera rotation muter" map??? (sesuai rubrik)
+========= TODO =========
+*/
 
 class Main {
   static init() {
     var canvasRef = document.getElementById("canvas");
 
     this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
 
-    this.camera.position.set(0, 0, 2);
+    this.camera1 = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera1.position.set(0, 50, 2);
+
+    this.camera2 = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera2.position.set(100, 50, 350);
+
+    this.camera3 = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera3.position.set(-100, 50, -350);
+
+    this.camera4 = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera4.position.set(50, 20, 50);
 
     this.renderer = new THREE.WebGLRenderer({
       antialias: true,
@@ -27,27 +57,30 @@ class Main {
     document.body.appendChild(this.renderer.domElement);
 
     // Setup free camera
-    this.freeCam = new FreeCam(this.camera, this.scene, this.renderer.domElement);
+    this.freeCam = new FreeCam(this.camera1, this.scene, this.renderer.domElement);
 
-    // Pointer lock setup
+    // Setup static camera
+    this.sceneCam1 = new SceneCam1(this.camera2, this.scene, this.renderer.domElement);
+    this.sceneCam2 = new SceneCam2(this.camera3, this.scene, this.renderer.domElement);
+    this.sceneCam3 = new SceneCam3(this.camera4, this.scene, this.renderer.domElement);
+
     this.domElement = this.renderer.domElement;
-    this.domElement.addEventListener('click', () => {
+    this.domElement.addEventListener("click", () => {
       this.domElement.requestPointerLock();
     });
 
-    document.addEventListener('pointerlockchange', () => {
+    document.addEventListener("pointerlockchange", () => {
       if (document.pointerLockElement === this.domElement) {
-        this.freeCam.enable();
+        this.enableCurrentCamera();
       } else {
-        this.freeCam.disable();
+        this.disableAllCameras();
       }
     });
 
+    document.addEventListener("keydown", this.onKeyDown.bind(this), false);
+
     // Plane
-    var plane = new THREE.Mesh(
-      new THREE.PlaneGeometry(40, 40),
-      new THREE.MeshPhongMaterial({ color: 0xa29979 })
-    );
+    var plane = new THREE.Mesh(new THREE.PlaneGeometry(40, 40), new THREE.MeshPhongMaterial({ color: 0xa29979 }));
 
     plane.rotation.set(-Math.PI / 2, 0, 0);
     plane.position.set(0, -3, 0);
@@ -56,7 +89,7 @@ class Main {
     this.scene.add(plane);
 
     // Lights
-    const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+    const ambientLight = new THREE.AmbientLight(0xffffff);
     this.scene.add(ambientLight);
 
     const hemisphereLight = new THREE.HemisphereLight(0xb1e1ff, 0xb97a20, 0.1);
@@ -72,10 +105,50 @@ class Main {
 
     // Load the map
     createMap(this.scene);
+
+    // Set the current camera
+    this.currentCamera = this.camera1;
+    this.currentController = this.freeCam;
+    this.enableCurrentCamera();
+  }
+
+  static enableCurrentCamera() {
+    if (this.currentController) {
+      this.currentController.enable();
+    }
+  }
+
+  static disableAllCameras() {
+    this.freeCam.disable();
+    this.sceneCam1.disable();
+  }
+
+  static onKeyDown(event) {
+    switch (event.code) {
+      case "Digit1":
+        this.switchToCamera(this.camera1, this.freeCam);
+        break;
+      case "Digit2":
+        this.switchToCamera(this.camera2, this.sceneCam1);
+        break;
+      case "Digit3":
+        this.switchToCamera(this.camera3, this.sceneCam2);
+        break;
+      case "Digit4":
+        this.switchToCamera(this.camera4, this.sceneCam3);
+        break;
+    }
+  }
+
+  static switchToCamera(camera, controller) {
+    this.disableAllCameras();
+    this.currentCamera = camera;
+    this.currentController = controller;
+    this.enableCurrentCamera();
   }
 
   static render(dt) {
-    this.renderer.render(this.scene, this.camera);
+    this.renderer.render(this.scene, this.currentCamera);
   }
 }
 
@@ -85,6 +158,9 @@ Main.init();
 function animate() {
   const delta = clock.getDelta();
   Main.freeCam.update(delta);
+  Main.sceneCam1.update(delta);
+  Main.sceneCam2.update(delta);
+  Main.sceneCam3.update(delta);
 
   Main.render(delta);
   requestAnimationFrame(animate);
---
+++ sceneCam1.js
@@ -0,0 +1,52 @@
+import * as THREE from "three";
+
+export class SceneCam1 {
+  constructor(camera, scene, domElement) {
+    this.camera = camera;
+    this.scene = scene;
+    this.domElement = domElement;
+    this.lookSpeed = 0.002;
+
+    this.enabled = false;
+
+    this.rotation = new THREE.Euler(0, 0, 0, "YXZ");
+    this.quaternion = new THREE.Quaternion();
+
+    this.initEventListeners();
+  }
+
+  enable() {
+    this.enabled = true;
+  }
+
+  disable() {
+    this.enabled = false;
+  }
+
+  initEventListeners() {
+    this.domElement.addEventListener("mousemove", this.onMouseMove.bind(this), false);
+  }
+
+  onMouseMove(event) {
+    if (!this.enabled) return;
+
+    const deltaX = -event.movementX * this.lookSpeed;
+    const deltaY = -event.movementY * this.lookSpeed;
+
+    // Apply the rotations using quaternions
+    this.quaternion.setFromEuler(this.rotation);
+    const quaternionX = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), deltaX);
+    const quaternionY = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), deltaY);
+
+    this.quaternion.multiplyQuaternions(quaternionX, this.quaternion);
+    this.quaternion.multiplyQuaternions(this.quaternion, quaternionY);
+
+    this.rotation.setFromQuaternion(this.quaternion);
+
+    this.camera.quaternion.copy(this.quaternion);
+  }
+
+  update(delta) {
+    if (!this.enabled) return;
+  }
+}
